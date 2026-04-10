243ba110358b7ee20c8c1102cd31b335fe73db6b
neetesh566 <shakyaneetesh2@gmail.com>
2026-04-10 23:32:54 +0530

    Created aiservice endpoints

---
+++ aiservice/aiservice/src/main/java/com/fitness/aiservice/config/MongoConfig.java
@@ -0,0 +1,13 @@
+package com.fitness.aiservice.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig  {
+
+}
