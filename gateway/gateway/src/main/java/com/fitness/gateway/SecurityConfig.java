d6decdc49965f719d06a2c8716eccd88caf8ad9b
neetesh566 <shakyaneetesh2@gmail.com>
2026-04-16 23:49:24 +0530

    Securing api gateway with keycloak

---
+++ gateway/gateway/src/main/java/com/fitness/gateway/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.fitness.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+import java.util.concurrent.Exchanger;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http){
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(Exchanger ->Exchanger.anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2-> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+}
